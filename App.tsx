7160fb51dd21e17d379ebb9d2feb6848b75dd352
Khang Nguyen Nhat <khangnl24112002@gmail.com>
2024-01-29 13:05:02 +0700

    first commit

---
+++ App.tsx
@@ -0,0 +1,16 @@
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return <View></View>;
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
